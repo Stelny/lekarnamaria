397ecc52fddbbabfa0d7e74d0b4f96e573c0fcdb
jakubstellner <jakub.stellner03@gmail.com>
2021-08-26 00:28:19 +0200

    Katalog

--- js/javascript.js
+++ js/javascript.js
@@ -196,5 +196,71 @@ $(document).ready(function () {
   navsCarousel(news, '.news-nav');
   
 
+  $('.active-menu').click(function(e){
+    e.preventDefault();
+    const drop = $(this).siblings(".drop-menu");
+    if (!drop.hasClass('active')) {
+      $('.drop-menu').removeClass('active');
+      drop.addClass('active');
+    } else {
+      $('.drop-menu').removeClass('active');
+    }
+    
+    
+  })
+
+
+    $(function () {
+
+      $(".category__filters--heading").click(function(){ $(this).siblings(".category__filters").toggle()  });
+      $(".category__filters--heading").click(function(){ $(this).toggleClass("rotateDeg") });
+
+      // Range slider
+      var rangeSlider = $("#range-slider");
+
+      rangeSlider.each(function () {
+      noUiSlider.create(this, {
+      start: [7120, 14160],
+      tooltips: false,
+      step: 150,
+      connect: true,
+      range: {
+      'min': [0],
+      'max': [10000]
+      },
+      format: {
+      to: function ( value ) {
+      value = Math.round(value);
+       return value+"€";
+      },
+      from: function ( value ) {
+        return value.replace(',-', '');
+      }
+      }
+      });
+      });
+
+      var priceRangeSlider = document.getElementById('range-slider');
+
+      priceRangeSlider.noUiSlider.on('update', function(values, handle) {
+        var value = values[handle];
+
+        if (handle) {
+        var right = value;
+        } else {
+        var left = value;
+        }
+
+        $("#slider-price-min").text(left);
+        $("#slider-price-max").html(right);
+
+      });
+    })
+    
+    $('.filter-btn').click(function(e) {
+      e.preventDefault();
+      $("body").addClass('filter');
+    })
+      
     
 });
